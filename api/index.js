2a9f66dac8857f83110023f024c44cb499f8d2e1
hilmanski <hilman@hilman.com>
2021-06-22 11:00:27 +0800

    allow CORS

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const websiteLogo = require( 'website-logo' )
 module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  
   const uri = req.query.url
 
   websiteLogo( uri, function( error, info ) {
